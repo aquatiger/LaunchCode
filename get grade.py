4b3e1834a10b1eb2da1151a1790fa4624166d5cc
aquatiger <rls4jc@gmail.com>
2018-04-05 17:10:06 -0700

    Another exercise
    I'm a little proud of this exercise, just because I got this working correctly the first time, unlike so many (read: all) other exercises.

---
+++ get grade.py
@@ -0,0 +1,24 @@
+def getGrade(grade):
+       #your code here
+    if grade >=90:
+        print('A')
+    elif grade >=80 and grade <90:
+        print('B')
+    elif grade >=70 and grade <80:
+        print('C')
+    elif grade >=60 and grade <70:
+        print('D')
+    else:
+        print('F')
+        
+getGrade(90)
+getGrade(89.99999)
+getGrade(80)
+getGrade(79.99999)
+getGrade(70)
+getGrade(69.99999)
+getGrade(60)
+getGrade(59.9999999)
+getGrade(50)
+getGrade(50000)
+
