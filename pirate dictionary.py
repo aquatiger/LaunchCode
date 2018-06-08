fc6975321523bd96eddd7a491903590e3a1e8b8a
aquatiger <rls4jc@gmail.com>
2018-06-07 20:05:10 -0700

    Work
    Today's work

---
+++ pirate dictionary.py
@@ -0,0 +1,7 @@
+"""
+Pirate dictionary
+Have user input sentence and have computer translate from English into Pirate
+
+"""
+
+pirate = {'sir': "matey", 'hotel': 'fleabag inn',  'student': 'swabie', 
--- yet another word count.py
+++ yet another word count.py
@@ -1,9 +1,24 @@
+"""
+remove punctuation
+count words in a file
+use dictionary for word count of each word in alphabetical order
+
+"""
+
+import string
 
 gettysburg = open("C:/Users/Roger/Documents/GitHub/LaunchCode/gettysburg.txt", "r")
+
+puncless = ""
+for char in gettysburg:
+    if char in string.punctuation:
+        gettysburg.replace(char, "")
+    else:
+        puncless += char
+print(puncless)
 
 address = []
 for line in gettysburg:
     splitted = line.split()
-    address.append(splitted)
-
+    address += splitted
 print(address)
