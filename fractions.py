c4d5a2ca44697a68fe8b6ab31e819f08caa9d0ea
aquatiger <rls4jc@gmail.com>
2018-08-13 15:07:15 -0700

    Update
    Got better results

--- fractions.py
+++ fractions.py
@@ -5,7 +5,7 @@ partially pseudocode
 """
 
 import math
-import numpy as numpy
+#import numpy as numpy
 
 fractions = [(0, 0, 0)]
 
@@ -18,9 +18,30 @@ for i in range(1, 10):
     sortedFrac =  sorted(fractions)
 
 print(sortedFrac)
+noDupFrac = [(0,0,0)]
 
-for i in range(len(fractions)):
-    print(sortedFrac[i][0])
+for i in range(len(sortedFrac)):
+    try:
+        if sortedFrac[i][0] == sortedFrac[i-1][0]: # so if the float equals the previous float
+            sortedFrac.remove(sortedFrac[i][0])           # remove the second float
+        else:
+            sortedFrac.append(sortedFrac[i][0])
+    except ValueError:
+        continue
+
+print(noDupFrac)
+"""
+The previous for loop works except for the fact it removes all the ones that are equal,
+not just the duplicates.
+"""
+    
+"""
+for the previous line got TypeError: integer argument expected, got float
+
+for previous line using remove instead of pop got ValueError: list.remove(x): x not in list
+
+"""
+print(sortedFrac)
 
 ##newFrac = []
 ##
