9ec6920ac6cbcfae891e1fdbbd44b4ef7404c1b5
aquatiger <rls4jc@gmail.com>
2018-08-21 15:48:18 -0700

    Update
    Continued to perfect this program. added the try and except loop and the last part which is the very reason I thought of this

--- fractions test.py
+++ fractions test.py
@@ -9,14 +9,15 @@ divided = numerator / denominator
 
 fractions = {}
 
+# this generates a dictionary with values in a list of the numerator and the denominator
 for i in range(0, 10):
     for j in range(1, 10):
         if i < j:
             x = i/j
             if x not in fractions:
-                fractions[x] = (i, j)
-                
-print(fractions)
+                fractions[x] = list((i, j))
+
+# takes the keys of the dictionary turns it into a list so I can sort it
 frackey = list(fractions.keys())
 frackey.sort()
 print(frackey)
@@ -24,16 +25,33 @@ print(frackey)
 ranges = []
 
 for i in range(len(frackey)):
-    a = frackey[i-1]
-    b = frackey[i] # b is the same as one of the keys in fractions
-    c = frackey[i+1]
+    try:
+        a = frackey[i-1]
+        b = frackey[i] # b is the same as one of the keys in fractions
+        c = frackey[i+1]
+        y = (b - ((b-a)/2)) # takes the previous float, divides by 2 and subtracts from i
+        z = ((c-b)/2) + b    # takes the next float, divides by 2 and adds to i
+        ranges.append((b, y, z))
+        print(ranges)
+    except IndexError: # needed to put this in because c is out of range otherwise
+        y = (b - ((b-a)/2))
+        z = ((1-b)/2) + b
+        ranges.append((b, y, z))
+        print(ranges)
 
-    y = (b - ((b-a)/2))
-    z = ((c-b)/2) + b
-    ranges.append((b, y, z))
-    print(ranges)
+print(fractions)
 
-##if divided > y and divided < z:
+for i in range(len(ranges)):
+    if divided < ranges[0][2]:
+        print("Your number", numerator, "/", denominator, "is approximately 0")
+        break
+    elif divided > ranges[i][1] and divided < ranges[i][2]:
+        print("Your number" + numerator +"/" + denominator + "is approximately" + fractions.values()[0] + " / " + fractions.values()[1])
+    elif divided > ranges[-1][2]:
+        print("Your number", numerator, "/", denominator, "is approximately 1")
+        break
+    
+    
 ##    fractions.ranges[b]
 ##    pseudocode = first value in the tuple of ranges that matches of the key of fractions
 ##          take the first of the tuple of values
