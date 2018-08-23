4846b0b28fe534b488fc38492c35a923df6377d5
aquatiger <rls4jc@gmail.com>
2018-08-22 18:05:34 -0700

    Work
    Continued work and testing on fhis program

--- fractions test.py
+++ fractions test.py
@@ -17,6 +17,9 @@ for i in range(0, 10):
             if x not in fractions:
                 fractions[x] = list((i, j))
 
+frackey = [(i, j) for i in range(0,10) for j in range (1,10)]
+print(frackey)
+
 # takes the keys of the dictionary turns it into a list so I can sort it
 frackey = list(fractions.keys())
 frackey.sort()
@@ -32,7 +35,6 @@ for i in range(len(frackey)):
         y = (b - ((b-a)/2)) # takes the previous float, divides by 2 and subtracts from i
         z = ((c-b)/2) + b    # takes the next float, divides by 2 and adds to i
         ranges.append((b, y, z))
-        print(ranges)
     except IndexError: # needed to put this in because c is out of range otherwise
         y = (b - ((b-a)/2))
         z = ((1-b)/2) + b
@@ -42,11 +44,13 @@ for i in range(len(frackey)):
 print(fractions)
 
 for i in range(len(ranges)):
+    print(i)
     if divided < ranges[0][2]:
         print("Your number", numerator, "/", denominator, "is approximately 0")
         break
     elif divided > ranges[i][1] and divided < ranges[i][2]:
-        print("Your number" + numerator +"/" + denominator + "is approximately" + fractions.values()[0] + " / " + fractions.values()[1])
+        print(fractions[frackey[i][0]])
+        #print("Your number", numerator, "/", denominator, "is approximately", fractions.get(), " / ", fractions.get())
     elif divided > ranges[-1][2]:
         print("Your number", numerator, "/", denominator, "is approximately 1")
         break
